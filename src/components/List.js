83a2de171ecbd0eb5f7c1a39f940858259ad3f17
Rajatsingh1997 <rajat@excellencetechnologies.info>
2020-10-21 11:02:09 +0530

    code formatting

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import './List.css';
+import React from "react";
+import "./List.css";
 
 function List(props) {
-    return (
-        <>
-          
-            <li>
-                <div className="list">
-                    <div><input type="checkbox" id={props.index} 
-                    onChange={() => { props.statusTodo(props.todos.id) }} 
-                    checked={props.todos.status} />
-                </div>
-                    <div className="text"><p>{props.todos.title}</p></div>
-                    <span className={props.todos.status ? "dot" : "dotred"}></span>
-                </div>
-                
-                <div className="horizontal"><hr/>
-                </div>
-            </li>
-                
-        </>
-    )
+  return (
+    <>
+      <li>
+        <div className="list">
+          <div>
+            <input
+              type="checkbox"
+              id={props.index}
+              onChange={() => {
+                props.statusTodo(props.todos.id);
+              }}
+              checked={props.todos.status}
+            />
+          </div>
+          <div className="text">
+            <p>{props.todos.title}</p>
+          </div>
+          <span className={props.todos.status ? "dot" : "dotred"}></span>
+        </div>
+
+        <div className="horizontal">
+          <hr />
+        </div>
+      </li>
+    </>
+  );
 }
 
 export default List;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,72 +1,70 @@
-import React, { useState } from 'react'
-import Inputform from './Inputform'
-import { CgAdd } from 'react-icons/cg';
-import List from './List'
-import './Todo.css'
+import React, { useState } from "react";
+import Inputform from "./Inputform";
+import { CgAdd } from "react-icons/cg";
+import List from "./List";
+import "./Todo.css";
 
 function Todo() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  //adding todo
 
-    //adding todo
+  const addTodo = (todo) => {
+    const newTodos = [
+      ...todos,
+      { title: todo, id: Math.random(), status: false },
+    ];
+    setTodos(newTodos);
+  };
+  //status
 
-    const addTodo = (todo) => {
-        const newTodos = [...todos,
-        {
-            title: todo,
-            id: Math.random(),
-            status: false
-        }];
-        setTodos(newTodos);
-    }
-    //status
+  const changeStatus = (todoId) => {
+    const newTodos = todos.map((item) => {
+      if (item.id == todoId) {
+        return { ...item, status: !item.status };
+      } else {
+        return item;
+      }
+    });
 
-    const changeStatus = (todoId) => {
-        console.log(todoId)
-        const newTodos = todos.map(item => {
-            if (item.id == todoId) {
-                return { ...item, status: !item.status }
-            } else {
-                return item;
-            }
-        })
-        console.log(newTodos)
-        setTodos(newTodos);
-    }
-    return (
-        <div className='maindiv'>
-            <div className='top'>
-                <h1>Today</h1>
-                <span className="btn"><CgAdd className="plus" size="25px"
-                    onClick={handleShow} /></span>
-            </div>
+    setTodos(newTodos);
+  };
+  return (
+    <div className="maindiv">
+      <div className="top">
+        <h1>Today</h1>
+        <span className="btn">
+          <CgAdd className="plus" size="25px" onClick={handleShow} />
+        </span>
+      </div>
 
-            <Inputform
-                handleClose={handleClose}
-                handleShow={handleShow}
-                show={show}
-                addTodo={addTodo}
-
-            />
-            <div className="content">
-                <div>
-                    <ul>
-                        {todos.map((todo, id) => {
-                            return <List
-                                key={id}
-                                index={id}
-                                todos={todo}
-                                statusTodo={changeStatus}
-                            />
-                        })}
-                    </ul>
-                </div>
-            </div>
+      <Inputform
+        handleClose={handleClose}
+        handleShow={handleShow}
+        show={show}
+        addTodo={addTodo}
+      />
+      <div className="content">
+        <div>
+          <ul>
+            {todos.map((todo, id) => {
+              return (
+                <List
+                  key={id}
+                  index={id}
+                  todos={todo}
+                  statusTodo={changeStatus}
+                />
+              );
+            })}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
